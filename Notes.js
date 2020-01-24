c1bd7d0aea8413c97ad317405106a902251afe5e
Ophelia Liu <ophelialiuyj@gmail.com>
2020-01-24 16:50:07 -0500

    lab1_complete

---
+++ Notes.js
@@ -0,0 +1,13 @@
+var fmap = function(arr, func){
+  var newArr = [];
+  for (var i = 0; i < arr.length; i = i + 1){
+    newArr.push(func(arr[i]));
+  }
+  return newArr;
+};
+
+const timestwo = function (num){return num*2};
+
+doubled = fmap([1, 2, 3, 4, 5], timestwo);
+
+"abc123;abc123".split("2") 
